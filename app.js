e8c634455243ed37a49ce2536e0a3a7e4d99c0f6
hillhousedev <adesemoyeolusola@gmail.com>
2020-11-19 18:44:18 +0100

    Added grayscale switcher etc

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const mapContainer = document.getElementById('map');
+
 var map = L.map('map').setView([39.742043, -104.991531], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -5,11 +7,25 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 const points = [];
+let bakedBeans = 0;
 let polyline;
+let mode = 'pink';
+mapContainer.classList.add('pink');
+const modes = ['pink', 'dark'];
+
 let bean = `https://static-cdn.jtvnw.net/emoticons/v1/302039277/4.0`
 
 
 map.on('click', (event) => {
+    if (mode === 'pink') {
+        mapContainer.classList.toggle(mode);
+        mode = 'dark';
+        mapContainer.classList.toggle(mode);
+    } else {
+        mapContainer.classList.toggle(mode);
+        mode = 'pink';
+        mapContainer.classList.toggle(mode);
+    }
     const latlng = [event.latlng.lat, event.latlng.lng];
     points.push(latlng);
     console.log(points);
@@ -28,14 +44,21 @@ map.on('click', (event) => {
     } else {
         polyline.setLatLngs(points);
         if (Math.random() < 0.2) {
+            bakedBeans++;
             L.popup({
                     autoClose: false,
                     closeOnClick: false,
                 })
                 .setLatLng(latlng)
                 .setContent(`
+                <h1>TOTAL: ${bakedBeans} </h1>
                 <h1>Start on trail head, <br>hike until you happy, <br>then you drive back home.</h1> 
-                <img class="baked-beans" src="https://static-cdn.jtvnw.net/emoticons/v1/302039277/4.0">
+                <div class="burried-treasure">
+                    <img class="burried-treasure" src="http://i.imgur.com/4EzXhBE.png">
+                    <img class="baked-beans" src="https://static-cdn.jtvnw.net/emoticons/v1/302039277/4.0">
+                    
+                </div>
+                
                 `)
                 .openOn(map);
 
