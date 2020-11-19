36f85e12d39c238afedfd881f0f09fb2343b3964
hillhousedev <adesemoyeolusola@gmail.com>
2020-11-19 15:48:59 +0100

    Added map to page

---
+++ app.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([39.742043, -104.991531], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
